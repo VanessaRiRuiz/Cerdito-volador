4924d322fe7a7e979b3765f6c617b7b18723b3aa
VanessaRiRuiz <vanerivera11.vrr@gmail.com>
2021-12-05 22:42:35 -0700

    new clouds

--- src/CloudH.cc
+++ src/CloudH.cc
@@ -5,7 +5,7 @@ float scale, float width, float height, int col, int row,
 sf::RenderWindow*& window, b2World*& world) :
 WallObject(textureUrl, position, scale, width, height, col, row, b2BodyType::b2_staticBody, window, world)
 {
-  idleAnim = new Animation(0.20f, 11,3, 5, 28, 10, drawable);
+  idleAnim = new Animation(0.20f, 14,4, 3, 23, 9, drawable);
 }
 
 CloudH::~CloudH()
